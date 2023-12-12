6020d03fb7d59eb5273b219b3e5e130a1abf5c75
hbfreelanceadministrative <118858976+hbfreelanceadministrative@users.noreply.github.com>
2023-12-12 18:52:05 +0100

    Update validate.js

--- assets/vendor/php-email-form/validate.js
+++ assets/vendor/php-email-form/validate.js
@@ -56,7 +56,11 @@
       headers: {'X-Requested-With': 'XMLHttpRequest'}
     })
     .then(response => {
+      if( response.ok ) {
       return response.text();
+        } else {
+        throw new Error(`${response.status} ${response.statusText} ${response.url}`); 
+      }
     })
     .then(data => {
       thisForm.querySelector('.loading').classList.remove('d-block');
